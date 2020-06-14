f27238a7135f32f710adeadaf5e6d389d5ab39e0
usukhbayar otgonbat <capusukh16@gmail.com>
2020-06-14 13:23:11 +0800

    DOM iig event listener tei holboh

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
+/*
+inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+*/
+var uiController = (function () {
+  // DOM ийн элементүүдийг тусад нь хувьсагчид хийж хадгална
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // Санхүүтэй ажиллах контроллер
 var fnController = (function () {})();
 
 // Програмын холбогч контроллер
 var appController = (function (parameter1, parameter2) {
+  var DOM = uiController.getDOMstrings();
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Оруулах өгөгдлийг дэлгэцээс олж авна");
+    console.log(uiController.getInput());
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
     // 4. Төсвийг тооцоолно
